8514744114ad42984beda76bcd1f2b86c6e17955
pranav2805 <pranavpradeep095@gmail.com>
2023-05-29 12:25:16 +0530

    Cart with many to many relationship

--- util/database.js
+++ util/database.js
@@ -1,6 +1,6 @@
 const Sequelize = require('sequelize');
 
-const sequelize = new Sequelize('node-complete', 'root', 'nodecomplete', {
+const sequelize = new Sequelize('node-complete', 'root', 'qwertyuiop', {
   dialect: 'mysql',
   host: 'localhost'
 });
